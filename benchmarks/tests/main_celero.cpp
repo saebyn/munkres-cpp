5a3b12d7dd29b11b6ab142b1581b086bd49dc2a6
Gluttton <gluttton@ukr.net>
2014-10-03 23:46:19 +0300

    Minor refactoring and removing extra code in benchmark based on using Celero.

--- benchmarks/tests/main_celero.cpp
+++ benchmarks/tests/main_celero.cpp
@@ -15,7 +15,7 @@ class MunkresFixture : public celero::TestFixture
     public:
         MunkresFixture  () = default;
         ~MunkresFixture () = default;
-        virtual std::vector <int64_t> getExperimentValues () const
+        virtual std::vector <int64_t> getExperimentValues () const override
         {
             std::vector <int64_t> problemSpace;
             for (int64_t i = 0; i < matrices.size (); ++i) {
@@ -24,12 +24,12 @@ class MunkresFixture : public celero::TestFixture
             return problemSpace;
         }
 
-        virtual void setUp (int64_t experimentValue)
+        void setUp (int64_t experimentValue) override
         {
             matrix = * matrices [experimentValue];
         }
 
-        virtual void tearDown ()
+        void tearDown () override
         {
         }
 
@@ -52,24 +52,6 @@ BENCHMARK_F (Munkres, Solve, MunkresFixture, 10, 500)
     Munkres munkres;
     auto m = this->matrix;
     munkres.solve (m);
-}
-
-
-
-BASELINE (Muknres, Solve, 5, 10)
-{
-    Munkres munkres;
-    auto matrix = * matrices [0];
-    munkres.solve (matrix);
-}
-
-
-
-BENCHMARK (Muknres, Solve, 5, 10)
-{
-    Munkres munkres;
-    auto matrix = * matrices [0];
-    munkres.solve (matrix);
 }
 
 
